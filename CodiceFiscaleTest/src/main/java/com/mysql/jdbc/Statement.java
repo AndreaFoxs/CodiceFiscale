22606df0e66b89e08231de6fb5d7a2a036426976
Drums <Drums@Drums-PC>
2017-06-26 17:52:03 +0200

    Progetto Andrea Volpes Codice Fiscale.

---
+++ CodiceFiscaleTest/src/main/java/com/mysql/jdbc/Statement.java
@@ -0,0 +1,12 @@
+package com.mysql.jdbc;
+
+import java.sql.ResultSet;
+
+public class Statement {
+
+	public ResultSet executeQuery(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
